1c3bd4a91d4ba7e52e5f27433032f03c65db7a09
Hikmah04525 <hikushhikma@gmail.com>
2025-11-18 21:53:08 +0300

    Add MongoDB connection, middleware setup, and server improvements

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require("express");
+/* const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require('dotenv');
 dotenv.config();
@@ -14,5 +14,42 @@ connectMongodb();
 app.use(express.json({ limit: "500mb"}));
 app.use(bodyParser.urlencoded({ limit: "500mb", extended:true }));
 
+module.exports = app; */
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const connectMongodb = require("./init/mongodb");
+
+const app = express();
+
+// Connect DB
+connectMongodb();
+
+// Middleware
+app.use(cors());
+app.use(express.json({ limit: "500mb" }));
+app.use(bodyParser.urlencoded({ limit: "500mb", extended: true }));
+
+// Test route
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
+// 404 handler
+app.use((req, res) => {
+    res.status(404).json({ message: "Route Not Found" });
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: "Server Error" });
+});
+
 module.exports = app;
+
 
--- init/mongodb.js
+++ init/mongodb.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+/*const mongoose = require("mongoose");
 const {connectionUrl} = require("../config/keys");
 
 const connectMongodb = async () => {
@@ -11,4 +11,40 @@ const connectMongodb = async () => {
     }
 };
 
-module.exports = connectMongodb
+module.exports = connectMongodb */
+
+const mongoose = require("mongoose");
+const { connectionUrl } = require("../config/keys");
+
+const connectMongodb = async () => {
+    console.log("Trying to connect to MongoDB...");
+
+    try {
+        await mongoose.connect(connectionUrl);
+
+        console.log("Database connection successful");
+    } catch (error) {
+        console.log("MongoDB Error:", error.message);
+    }
+
+    mongoose.connection.on("connected", () => {
+        console.log("Mongoose connected");
+    });
+
+    mongoose.connection.on("error", (err) => {
+        console.log("Mongoose error:", err);
+    });
+
+    mongoose.connection.on("disconnected", () => {
+        console.log("Mongoose disconnected");
+    });
+
+    // Graceful Shutdown
+    process.on("SIGINT", async () => {
+        await mongoose.connection.close();
+        console.log("Mongoose closed on app termination");
+        process.exit(0);
+    });
+};
+
+module.exports = connectMongodb;
